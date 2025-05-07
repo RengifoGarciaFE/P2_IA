c215e6fac1accd9d21fe78d2471d8fcc32a32c7d
Ana Ordóñez <146625596+anaxrdonez@users.noreply.github.com>
2025-05-07 19:36:39 +0200

    comentando
    qmindtrainer

--- Assets/Scripts/GrupoB/QMindTrainer.cs
+++ Assets/Scripts/GrupoB/QMindTrainer.cs
@@ -16,8 +16,9 @@ namespace GrupoB
         public int CurrentStep { get; private set; }
         public CellInfo AgentPosition { get; private set; }
         public CellInfo OtherPosition { get; private set; }
-        public float Return { get; private set; }
-        public float ReturnAveraged { get; private set; }
+        public float Return { get; private set; } //recompensa total
+        public float ReturnAveraged { get; private set; } //promedio
+        //para notificar cuand comienza o acaba un episodio
         public event EventHandler OnEpisodeStarted;
         public event EventHandler OnEpisodeFinished;
 
@@ -32,9 +33,9 @@ namespace GrupoB
 
         private bool terminal_state;
 
-        public float epsilon;
-        public float alpha;
-        public float gamma;
+        public float epsilon; //probab de explorar
+        public float alpha; //tasa de aprendizaje
+        public float gamma; //factor de descuento
 
         public void Initialize(QMindTrainerParams qMindTrainerParams, WorldInfo worldInfo, INavigationAlgorithm navigationAlgorithm)
         {
@@ -49,42 +50,43 @@ namespace GrupoB
             if (File.Exists(filePath))
             {
                 Debug.Log("Archivo de tabla Q encontrado. Cargando...");
-                _qTable.Load();
+                _qTable.Load(); //si existe una tabla Q la carga
             }
-
-            AgentPosition = worldInfo.RandomCell();
+            
+            //colocar al agente y enemigo en posiciones aleatorias del mundo
+            AgentPosition = worldInfo.RandomCell(); 
             OtherPosition = worldInfo.RandomCell();
-            OnEpisodeStarted?.Invoke(this, EventArgs.Empty);
+            OnEpisodeStarted?.Invoke(this, EventArgs.Empty); //lanza evento de inicio
 
             epsilon = _qMindTrainerParams.epsilon;
             alpha = _qMindTrainerParams.alpha;
             gamma = _qMindTrainerParams.gamma;
         }
 
-        public void DoStep(bool train)
+        public void DoStep(bool train) //bucle de entrenamiento (importante) / train solo para actualizar tabla o no 
         {
-            if (terminal_state)
+            if (terminal_state)//si el episodio anterior a terminado devuelve la media de recompensas lanza el evnto y resetea
             {
                 ReturnAveraged = ReturnAveraged * 0.9f + Return * 0.1f;
                 OnEpisodeFinished?.Invoke(this, EventArgs.Empty);
                 ResetEnvironment();
             }
 
-            State state = new State(AgentPosition, OtherPosition, _worldInfo);
-            int action = selectAction(state);
-            (CellInfo newAgentPos, CellInfo newEnemyPos) = UpdateEnvironment(action);
-            State nextState = new State(newAgentPos, newEnemyPos, _worldInfo);
-            float reward = CalculateReward(newAgentPos, newEnemyPos);
+            State state = new State(AgentPosition, OtherPosition, _worldInfo); //estado actual del entorno
+            int action = selectAction(state); //decide que acción tomar
+            (CellInfo newAgentPos, CellInfo newEnemyPos) = UpdateEnvironment(action); //aplicar acción elegida
+            State nextState = new State(newAgentPos, newEnemyPos, _worldInfo); //siguiente estado después de mover a ambos
+            float reward = CalculateReward(newAgentPos, newEnemyPos); //calcula la rescompensa obtenida por este paso
 
             totalReward += reward;
             Return = Mathf.Round(totalReward * 10f) / 10f;
 
-            if (train)
+            if (train) //si estamo entrenando no solo observando aplicaremos la formula de q-learning
             {
                 UpdateQtable(state, action, reward, nextState);
             }
 
-            AgentPosition = newAgentPos;
+            AgentPosition = newAgentPos; //actualizamos las posiciones para el siguiente paso
             OtherPosition = newEnemyPos;
             _stepCount++;
             CurrentStep = _stepCount;
@@ -94,36 +96,39 @@ namespace GrupoB
 
         private void ResetEnvironment()
         {
-            do { AgentPosition = _worldInfo.RandomCell(); } while (!AgentPosition.Walkable);
+            //colocar agente  y enemigo en celdas aleatorias que sean transitables y que el enem esté al menos a 3 celdas del agente
+            do { AgentPosition = _worldInfo.RandomCell(); } while (!AgentPosition.Walkable); 
             do { OtherPosition = _worldInfo.RandomCell(); } while (!OtherPosition.Walkable || AgentPosition.Distance(OtherPosition, CellInfo.DistanceType.Manhattan) < 3);
 
             terminal_state = false;
             _episodeCount++;
             CurrentEpisode = _episodeCount;
+            //reinicia contadores
             _stepCount = 0;
             CurrentStep = 0;
             totalReward = 0;
 
             if (_episodeCount % _qMindTrainerParams.episodesBetweenSaves == 0 || _episodeCount == _qMindTrainerParams.episodes)
-            {
+            { //cadaepisodeBetweenSaves o al llegar al ultimo episodio se guarda la tabla
                 _qTable.Save();
                 Debug.Log($"[Ep {_episodeCount}] Tabla Q guardada.");
             }
 
             if (epsilon > 0.1f)
             {
-                epsilon *= 0.999f;
-                epsilon = Mathf.Max(epsilon, 0.1f);
+                epsilon *= 0.999f; //con el tiempo explorará menos cuanto más ato
+                                   //más probabilidad de elegir una acción aleatoria 
+                epsilon = Mathf.Max(epsilon, 0.1f); //para que el epsilon no baje del 0.1
             }
 
             OnEpisodeStarted?.Invoke(this, EventArgs.Empty);
         }
 
-        private int selectAction(State state)
+        private int selectAction(State state) //genera un nº (0-1) si es menor o igual que epsilon 
         {
             return (Random.value <= epsilon)
-                ? Random.Range(0, _qTable.actions)
-                : _qTable.GetAction(state);
+                ? Random.Range(0, _qTable.actions) //elegira una acción aleatoria (explorará)
+                : _qTable.GetAction(state); // sino elegirá la mehor acción aprendida (explotación)
         }
 
         private (CellInfo, CellInfo) UpdateEnvironment(int action)
@@ -135,11 +140,13 @@ namespace GrupoB
         }
 
         private void UpdateQtable(State state, int action, float reward, State nextState)
-        {
+        {//recibe estado actual, acción que se tomó desde el estado, recompensa recibida y estado siguiente al que se llega
             float actualQ = _qTable.GetQValue(state, action);
             float maxNextQ = _qTable.GetMaxQValue(nextState);
+
+            // Q(s,a) <-- (1 - ?)    *  Q(s,a) +   ?   * [ R     +  ?    * max Q(s',a')]
             float newQ = (1 - alpha) * actualQ + alpha * (reward + gamma * maxNextQ);
-            _qTable.UpdateQValue(state, action, newQ);
+            _qTable.UpdateQValue(state, action, newQ); //guardar nuevo valor
         }
 
         private float CalculateReward(CellInfo newAgentPos, CellInfo newEnemyPos)
@@ -161,7 +168,7 @@ namespace GrupoB
 
         private void Update()
         {
-            // ? Entrenamiento rápido sin depender de "train"
+            // Entrenamiento rápido sin depender de "train"
             if (_qMindTrainerParams != null && Application.isPlaying)
             {
                 int stepsPerFrame = 100;
@@ -171,7 +178,7 @@ namespace GrupoB
                 }
             }
 
-            // ?? Guardado manual con tecla S
+            // Guardado manual con tecla S
             if (Input.GetKeyDown(KeyCode.S))
             {
                 _qTable.Save();
